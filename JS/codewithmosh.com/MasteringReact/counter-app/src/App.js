ef0a063f49d31e0a829efec1b2cd94cd5e0f1c3b
Usman Akram <usman.akram99@hotmail.com>
2019-08-07 08:10:47 +0500

    Learning exercise "counter-app" during "Mastering React" (codewithmosh.com)

---
+++ JS/codewithmosh.com/MasteringReact/counter-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+/* import NavBar from "./components/navbar";
+import Counters from "./components/counters"; */
+import Parent from "./components/parent";
+import "./App.css";
+
+function App() {
+  console.log("App - Rendered");
+  return <Parent />;
+}
+
+export default App;
