f5861907e88e25e401c83f35b2db5e20c9dededd
Usman Akram <usman.akram99@hotmail.com>
2019-10-29 00:02:05 +0500

    nodejs cource started

---
+++ JS/codewithmosh.com/TheCompleteNode.jsCourse/first-aap/logger.js
@@ -0,0 +1,8 @@
+function log(message) {
+  // Send an HTTP request
+  console.log(message);
+}
+
+// module.exports.log = log;
+// OR
+module.exports = log;
---
+++ JS/codewithmosh.com/TheCompleteNode.jsCourse/notes/lesson2.5-Loading a Module/app.js
@@ -0,0 +1,7 @@
+// const logger = require("./logger");
+// logger.log("hello world");
+
+// OR
+
+const log = require("./logger");
+log("hello world");
